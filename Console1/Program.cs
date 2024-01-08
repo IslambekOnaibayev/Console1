e297072506b9c15e3247670143d89d295e3c9158
Islambek Onaibayev <islambek.onaibayev@outlook.com>
2024-01-08 10:45:02 +0600

    Добавьте файлы проекта.

---
+++ Console1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Channels;
+
+namespace Console1
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var tom = new Person("Tomas", 32);
+            Console.WriteLine(tom.Name);    //Tomas
+            var bob = (Person)tom.Clone();
+            bob.Name = "Bobas";
+            Console.WriteLine(bob.Name);    //Bobas
+            Console.WriteLine(tom.Name);    //Bobas
+
+        }
+    }
+
+    public class Person: ICloneable
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person(string name, int age)
+        {
+            Name = name; Age = age;
+        }
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+
+
+}
+
+
+
+
+
+
